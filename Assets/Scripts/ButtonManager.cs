eae15dcd201c9f723c918d35d74ba5b76e071081
c00208743 <c00208743@itcarlow.ie>
2017-11-20 11:49:50 +0000

    Menu System and Level1

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	public void NewGameBtn (string newGameLevel) {
+        SceneManager.LoadScene(newGameLevel);
+	}
+}
