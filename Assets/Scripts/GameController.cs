d2e1f7862262f8524d46f665777023d14b344f39
rego363 <rego363@gmail.com>
2017-12-01 17:25:25 +0000

    Added code to delete rows

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+
+    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
+
     public GameObject tShape;
     public GameObject lShape;
     public GameObject mirroredLShape;
@@ -15,13 +20,109 @@ public class GameController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(Spawn());
+        //StartCoroutine(Spawn());
+        //SpawnNextTetromine();
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public bool IsFullRowAt(int y)
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            if(grid[x, y] == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void MoveRowDown(int y)
+    {
+        for(int x = 0; x < gridWidth; x++)
+        {
+            if(grid[x,y] != null)
+            {
+                grid[x, y - 1] = grid[x, y];
+                grid[x, y] = null;
+                grid[x, y - 1].position += new Vector3(0, -1, 0);
+            }
+        }
+    }
+
+    public void MoveAllRowsDown(int y)
+    {
+        for (int i = y; i < gridHeight; i++)
+        {
+            MoveRowDown(i);
+        }
+    }
+
+    public void DeleteMinoAt(int y)
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            Destroy(grid[x, y].gameObject);
+            grid[x, y] = null;
+        }
+    }
+
+    public void DeleteRow()
+    {
+        for(int y = 0; y < gridHeight; y++)
+        {
+            if(IsFullRowAt(y))
+            {
+                DeleteMinoAt(y);
+                MoveAllRowsDown(y + 1);
+                y--;
+            }
+        }
+    }
+
+    public void UpdateGrid(Shapemovement tetromino)
+    {
+        for (int y = 0; y < gridHeight; y++)
+        {
+            for (int x = 0; x < gridWidth; x++)
+            {
+                if(grid[x, y] != null)
+                {
+                    if(grid[x,y].parent == tetromino.transform)
+                    {
+                        grid[x, y] = null;
+                    }
+                }
+            }
+
+        }
+        
+        foreach (Transform mino in tetromino.transform)
+        {
+            Vector2 pos = new Vector2(Mathf.Round(mino.position.x), Mathf.Round(mino.position.y));
+            if(pos.y < gridHeight)
+            {
+                grid[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }
+
+    public Transform GetTransformAtGridPosition(Vector2 pos)
+    {
+        if (pos.y < gridHeight - 1)
+        {
+            return null;
+        }
+        else
+        {
+            return grid[(int)pos.x, (int)pos.y];
+        }
+    }
 
     IEnumerator Spawn()
     {
