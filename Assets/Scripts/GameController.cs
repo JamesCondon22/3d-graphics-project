2e87b3fb1a83335f8bed299d2f14b1376947ddc6
rego363 <rego363@gmail.com>
2017-12-01 14:49:30 +0000

    Spawning complete

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject tShape;
+    public GameObject lShape;
+    public GameObject mirroredLShape;
+    public GameObject lineShape;
+    public GameObject squareShape;
+    public GameObject sShape;
+    public GameObject zShape;
+    public float wait;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawn());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(wait);
+        while (true)
+        {
+            int whichPiece = Random.Range(0, 7);
+            if (whichPiece == 0)
+            {
+                Instantiate(tShape);
+            }
+            else if (whichPiece == 1)
+            {
+                Instantiate(lShape);
+            }
+            else if (whichPiece == 2)
+            {
+                Instantiate(mirroredLShape);
+            }
+            else if (whichPiece == 3)
+            {
+                Instantiate(lineShape);
+            }
+            else if (whichPiece == 4)
+            {
+                Instantiate(squareShape);
+            }
+            else if (whichPiece == 5)
+            {
+                Instantiate(sShape);
+            }
+            else
+            {
+                Instantiate(zShape);
+            }
+
+            yield return new WaitForSeconds(wait);
+        }
+    }
+}
